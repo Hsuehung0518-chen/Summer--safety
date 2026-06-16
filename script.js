5b06225b3ccbebb208170d95e3edfb764054f361
Hsuehung <seanchen0518@gmail.com>
2026-06-16 16:58:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function calculateScore() {
-  const checked = document.querySelectorAll("input:checked").length;
-  let score = 100 - checked * 25;
+  const checked = document.querySelectorAll("input[type=checkbox]:checked").length;
 
+  let score = 100 - checked * 25;
   let msg = "";
 
   if (score >= 75) msg = "✅ 很健康";
-  else if (score >= 50) msg = "⚠️ 稍微注意";
-  else msg = "🚨 建議改善";
+  else if (score >= 50) msg = "⚠️ 注意環境";
+  else msg = "🚨 需要改善";
 
   document.getElementById("result").innerHTML = `
-    <h3>健康指數：${score}</h3>
+    <p>健康指數：${score}</p>
     <p>${msg}</p>
-    <button onclick="goToApp()">交給 AI 幫你管理</button>
+    <button onclick="goToApp()">交給 AI 管理</button>
   `;
 }
 
